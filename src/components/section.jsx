6a8eb7e4a045d085d5de3183cb1cf3349ccfc45f
João Victor Calasans <142369193+jhonzz02@users.noreply.github.com>
2025-07-04 21:42:34 -0300

    feat tutorial section

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -7,8 +7,12 @@ export default function SectionTutorial() {
       <div className="flex gap-6 justify-between">
         {/* VIDEO */}
         <div className="w-1/2">
-          <img src={""} alt="" className="bg-white" />
+          <video className="w-full rounded-lg h-[476px]" controls autoPlay muted>
+            <source src="https://www.seusite.com/video.mp4" type="video/mp4" />
+            Seu navegador não suporta vídeo.
+          </video>
         </div>
+
         {/* TEXT */}
         <div className="w-1/2">
           <h2 className="text-4xl md:text-5xl font-bold mb-8 gradient-text ">
@@ -18,13 +22,17 @@ export default function SectionTutorial() {
             <div className="flex items-start space-x-4 group">
               <CheckCircle className="h-6 w-6 text-primary mt-1 flex-shrink-0 group-hover:scale-110 transition-transform" />
               <div>
-                <h3 className="text-xl font-semibold mb-2 ">Lembrete automatico do agendamento com o cliente</h3>
+                <h3 className="text-xl font-semibold mb-2 ">
+                  Lembrete automatico do agendamento com o cliente
+                </h3>
               </div>
             </div>
             <div className="flex items-start space-x-4 group">
               <CheckCircle className="h-6 w-6 text-primary mt-1 flex-shrink-0 group-hover:scale-110 transition-transform" />
               <div>
-                <h3 className="text-xl font-semibold mb-2 ">Atendimento 24/7</h3>
+                <h3 className="text-xl font-semibold mb-2 ">
+                  Atendimento 24/7
+                </h3>
               </div>
             </div>
             <div className="flex items-start space-x-4 group">
@@ -36,25 +44,33 @@ export default function SectionTutorial() {
             <div className="flex items-start space-x-4 group">
               <CheckCircle className="h-6 w-6 text-primary mt-1 flex-shrink-0 group-hover:scale-110 transition-transform" />
               <div>
-                <h3 className="text-xl font-semibold mb-2 ">Possibilidade de multiplas unidades</h3>
+                <h3 className="text-xl font-semibold mb-2 ">
+                  Possibilidade de multiplas unidades
+                </h3>
               </div>
             </div>
             <div className="flex items-start space-x-4 group">
               <CheckCircle className="h-6 w-6 text-primary mt-1 flex-shrink-0 group-hover:scale-110 transition-transform" />
               <div>
-                <h3 className="text-xl font-semibold mb-2 ">Taxas de clientes, agendamentos e mensagens</h3>
+                <h3 className="text-xl font-semibold mb-2 ">
+                  Taxas de clientes, agendamentos e mensagens
+                </h3>
               </div>
             </div>
             <div className="flex items-start space-x-4 group">
               <CheckCircle className="h-6 w-6 text-primary mt-1 flex-shrink-0 group-hover:scale-110 transition-transform" />
               <div>
-                <h3 className="text-xl font-semibold mb-2 ">Monitoramento ao vivo da qualidade do atendimento</h3>
+                <h3 className="text-xl font-semibold mb-2 ">
+                  Monitoramento ao vivo da qualidade do atendimento
+                </h3>
               </div>
             </div>
             <div className="flex items-start space-x-4 group">
               <CheckCircle className="h-6 w-6 text-primary mt-1 flex-shrink-0 group-hover:scale-110 transition-transform" />
               <div>
-                <h3 className="text-xl font-semibold mb-2 ">Entre diversas outras funcionalidades</h3>
+                <h3 className="text-xl font-semibold mb-2 ">
+                  Entre diversas outras funcionalidades
+                </h3>
               </div>
             </div>
           </div>
